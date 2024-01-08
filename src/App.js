8d04ec651580e8640bc8416a7fb453c2bb207647
Govind1411 <102456645+Govind1411@users.noreply.github.com>
2024-01-08 13:39:56 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/nav';
+
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <React.Fragment>
+        <NavBar/>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;
+
+
